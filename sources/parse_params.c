f9c43794b98c552d69e2e47b36d91523381db398
grvelva <amgboris@ya.ru>
2021-06-21 11:04:13 +0300

    добавил проверку параметров на инт

--- sources/parse_params.c
+++ sources/parse_params.c
@@ -1,9 +1,41 @@
 // #include "philo_one.h"
 #include "../includes/philo_one.h"
 
+int		is_int(char *line)
+{
+	int		i;
+
+	i = 0;
+	if (!line)
+		return(0);
+	while ((line[i] < 14 && line[i] > 8) || line[i] == 32)
+		i++;
+	while (line[i] == '+' || line[i] == '-')
+		i++;
+	while (line[i] >= '0' && line[i] <= '9')
+		i++;
+	return (!line[i]);
+	
+}
+
+int		check_params(char* argv[])
+{
+	int		i;
+	if (!argv)
+		return(0);
+	i = 1;
+	while (argv[i])
+	{
+		if (!is_int(argv[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int		parse_params(int argc, char *argv[], t_data *data)
 {
-	if (argc < 5 || argc > 6)
+	if (argc < 5 || argc > 6 || !check_params(argv))
 		return(0);
 	data->philos_count = ft_atoi(argv[1]);
 	if (data->philos_count < 2)
